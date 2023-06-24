5c3dd53a616a062dcef055538f2d7d748b432ec3
Rachel <gggggrea@gmail.com>
2023-06-24 12:53:01 +0100

    Learn some basic

---
+++ 001/server.js
@@ -0,0 +1,19 @@
+const os = require("os");
+const path = require("path");
+const { add, subtract, multiply, divide } = require("./math");
+
+console.log(add(2, 3));
+
+// console.log(1, os.type());
+// console.log(2, os.version());
+// console.log(3, os.homedir());
+// console.log(4, os.type());
+
+// console.log(5, __filename);
+// console.log(6, __dirname);
+
+// console.log(7, path.basename(__dirname));
+// console.log(8, path.basename);
+// console.log(9, path.dirname);
+// console.log(10, path.dirname(__dirname));
+// console.log(11, path.dirname(__filename));
