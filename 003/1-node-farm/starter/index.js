725d44719ed2d95a7acbdfadb83b185ef97488fd
Rachel <gggggrea@gmail.com>
2023-07-01 10:08:40 +0100

    Learn building API!

--- 003/1-node-farm/starter/index.js
+++ 003/1-node-farm/starter/index.js
@@ -1,9 +1,25 @@
 const fs = require("fs");
 const http = require("http");
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.end("Hello from the server!!!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
