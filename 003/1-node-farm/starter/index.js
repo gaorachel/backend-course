aa859752b4dfea8608f38e4cec337a5f945659c0
Rachel <gggggrea@gmail.com>
2023-07-01 21:42:45 +0100

    Learn slugify

--- 003/1-node-farm/starter/index.js
+++ 003/1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 const parseQuery = require("./modules/parseQuery");
 
@@ -10,6 +11,7 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
+const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   let { query, pathname: pathName } = url.parse(req.url, true);
