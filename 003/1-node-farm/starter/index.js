7a57b99b1911d4991f9f0b74940174eb579a54ca
Rachel <gggggrea@gmail.com>
2023-07-01 21:20:44 +0100

    Refactor code by using module.exports

--- 003/1-node-farm/starter/index.js
+++ 003/1-node-farm/starter/index.js
@@ -1,20 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%NOT_ORGANIC%}/g, product.organic ? "" : "not-organic");
-
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
+const parseQuery = require("./modules/parseQuery");
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
@@ -22,10 +10,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
-
-const parseQuery = (query) => {
-  return JSON.parse(JSON.stringify(query));
-};
 
 const server = http.createServer((req, res) => {
   let { query, pathname: pathName } = url.parse(req.url, true);
---
+++ 003/1-node-farm/starter/modules/parseQuery.js
@@ -0,0 +1,3 @@
+module.exports = (query) => {
+  return JSON.parse(JSON.stringify(query));
+};
