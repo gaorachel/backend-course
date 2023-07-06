fed6a8257dd46b61464bb92f0cfd6649a1f307b2
Rachel <gggggrea@gmail.com>
2023-07-06 17:25:52 +0100

    Work with URL

--- 003/4-natours/starter/app.js
+++ 003/4-natours/starter/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // middleware
+
 // app.get('/', (req, res) => {
 //   //   res.status(200).send('Hello from the server side');
 //   res
@@ -21,8 +23,50 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: { tours },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  // console.log(req.params);
+  // console.log(req.params.id);
+  const id = req.params.id * 1; // convert a string to a number
+  const tour = tours.find((el) => el.id === id);
+
+  if (id > tours.length) || (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+app.post('/api/v1/tours/', (req, res) => {
+  // console.log(req.body);
+  // res.send('Done');
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
