6dfdb17bcc6f52b9cbc970aa0670dcdfd58ca5a6
Rachel <gggggrea@gmail.com>
2023-07-08 20:34:21 +0100

    Param middleware and chain multiple middlewares

--- 003/4-natours/starter/controllers/tourController.js
+++ 003/4-natours/starter/controllers/tourController.js
@@ -5,6 +5,27 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'missing name or price',
+    });
+  }
+  next();
+};
+
+exports.checkID = (req, res, next, val) => {
+  console.log('Tour id is: ', val);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
@@ -19,13 +40,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1; // convert a string to a number
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -53,13 +67,6 @@ exports.createTours = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: { tour: 'This is a placeholder' },
@@ -67,13 +74,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     // 204 means no content - because after deleting, there is no data being sent back.
     status: 'success',
--- 003/4-natours/starter/routes/tourRoutes.js
+++ 003/4-natours/starter/routes/tourRoutes.js
@@ -5,11 +5,15 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  checkID,
+  checkBody,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(createTours);
+router.param('id', checkID);
+
+router.route('/').get(getAllTours).post(checkBody, createTours);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
